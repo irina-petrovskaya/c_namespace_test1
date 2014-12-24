09103c6a45fea2ae27740134d405732d2edc8910
user_ubuntu <irina@ubuntu.com>
2014-12-24 15:59:47 +0300

    a1.sss

--- src/mybeans/A2.java
+++ src/mybeans/A2.java
@@ -4,6 +4,7 @@ package mybeans;
  * Created by user on 24/12/14.
  */
 public class A2 extends A1 {
+    String sss;
     @Override
     public String toString() {
         return super.toString()+" overrided in A2{}";
