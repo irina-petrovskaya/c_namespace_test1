87ce05d8666aab16ce6505a26e7d19cce361b2bd
irina <irina@me.com>
2014-12-24 15:59:22 +0300

    a2

--- src/mybeans/A2.java
+++ src/mybeans/A2.java
@@ -3,5 +3,9 @@ package mybeans;
 /**
  * Created by user on 24/12/14.
  */
-public class A2 {
+public class A2 extends A1 {
+    @Override
+    public String toString() {
+        return super.toString()+" overrided in A2{}";
+    }
 }
