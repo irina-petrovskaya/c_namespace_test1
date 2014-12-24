bc21e387982d66c6da77a7bfb5ef6c7e88835226
irina <irina@me.com>
2014-12-24 16:33:27 +0300

    str1

--- src/mybeans/A2.java
+++ src/mybeans/A2.java
@@ -6,18 +6,17 @@ import x.y.Foo;
  * Created by user on 24/12/14.
  */
 public class A2 extends A1 {
-    String sss;
+    String str1;
 
+    public String getStr1() {
+        return str1;}
     public A2(int myint) {
         super(myint);
     }
 
-    public String getSss() {
-        return sss;
-    }
 
-    public void setSss(String sss) {
-        this.sss = sss;
+    public void setStr1(String str1) {
+        this.str1 = str1;
     }
     //
     @Override
