5a87d29503cdd4dc6375e38257d98c4d99bf23bb
user_ubuntu <irina@ubuntu.com>
2014-12-24 18:44:34 +0300

    baz

--- src/x/y/Baz.java
+++ src/x/y/Baz.java
@@ -4,4 +4,33 @@ package x.y;
  * by Irina.Petrovskaya, on 24.03.11, 16:13
  */
 public class Baz {
+
+    private String aaaa;
+    private String bbb;
+
+    public String getXxx() {
+        return xxx;
+    }
+
+    public void setXxx(String xxx) {
+        this.xxx = xxx;
+    }
+
+    public String getBbb() {
+        return bbb;
+    }
+
+    public void setBbb(String bbb) {
+        this.bbb = bbb;
+    }
+
+    public String getAaaa() {
+        return aaaa;
+    }
+
+    public void setAaaa(String aaaa) {
+        this.aaaa = aaaa;
+    }
+
+    private String xxx;
 }
