f8a71859d89d13d59c19e01714dcf7431c708c34
user_ubuntu <irina@ubuntu.com>
2014-12-24 16:00:08 +0300

    a1.sss accessors

--- src/mybeans/A2.java
+++ src/mybeans/A2.java
@@ -5,6 +5,14 @@ package mybeans;
  */
 public class A2 extends A1 {
     String sss;
+
+    public String getSss() {
+        return sss;
+    }
+
+    public void setSss(String sss) {
+        this.sss = sss;
+    }
     @Override
     public String toString() {
         return super.toString()+" overrided in A2{}";
