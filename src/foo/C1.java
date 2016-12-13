1469560463d1452c4612a2705ab9c96086ee46d8
user_ubuntu <irina@ubuntu.com>
2016-12-13 17:32:33 +0300

    C1.java: some dummy methods

--- src/foo/C1.java
+++ src/foo/C1.java
@@ -4,4 +4,18 @@ package foo;
  * Created by user on 13/01/15.
  */
 public class C1 {
+
+    public void method1(int a11, long a21, String a13, Integer a14, Long a15, boolean a16, Boolean a17, char a18 ){
+
+    }
+    public void method2(String ss1, String ss2, String ss3 ){
+
+    }
+
+    public void foo(){
+        this.method1(0,0,"aaa",0, Long.valueOf(0),true,false,'a');
+        this.method2("","","");
+    }
+
+
 }
